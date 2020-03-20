8cafceed4ba261e052c22a54762025a131fabd1c
julian <julian.harley@gmail.com>
2020-03-20 11:36:59 +0000

    start inventory app;

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from inventory.models import Inventory
+
+class TestInventory(TestCase):
+    def createInventory(self):
+        i = Inventory.objects.create(title='first_test')
+        i.save()
+        self.assertTrue(Inventory.objects.count(),1)
