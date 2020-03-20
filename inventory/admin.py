2db51f80ccddc3b6f9670e356a009472e4e5f6f2
julian <julian.harley@gmail.com>
2020-03-20 12:00:50 +0000

    populating inventory & make admin show images

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
+
 from inventory.models import Inventory
-from django.utils.html import mark_safe
 
 
 class InventoryAdmin(admin.ModelAdmin):
     fields = ['image_tag']
     readonly_fields = ['image_tag']
-    list_display = ['title','image_tag',]
+    list_display = ['title', 'image_tag', ]
 
 
 admin.site.register(Inventory, InventoryAdmin)
